6373f7ecaa8a9836c35f1ede7a722039312d673d
saiglor <Jakimets.2012@gmail.com>
2022-01-29 22:27:23 +0500

    Запуск

---
+++ Otus.Solid/Program.cs
@@ -0,0 +1,23 @@
+﻿using Otus.Solid.Abstractions;
+using Otus.Solid.Game;
+using Otus.Solid.IO;
+
+namespace Otus.Solid
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            IReader reader = new ConsoleReader();
+            IPrinter printer = new ConsolePrinter();
+
+            ISettings settings = new GameSettings()
+                .SetTotatNumberAttempts(3)
+                .SetMinNumber(50)
+                .SetMaxNumber(100);
+
+            var game = new GuessNumberGame(reader, printer, settings);
+            game.Start();
+        }
+    }
+}
