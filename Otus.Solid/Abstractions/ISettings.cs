03ae9b971e06b78566f814b83a536302d3e5284a
saiglor <Jakimets.2012@gmail.com>
2022-01-29 22:26:04 +0500

    Добавил Settings

---
+++ Otus.Solid/Abstractions/ISettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Otus.Solid.Abstractions
+{
+    public interface ISettings
+    {
+        int TotalNumberAttempts { get; }
+        int MinNumber { get; }
+        int MaxNumber { get; }
+
+        ISettings SetTotatNumberAttempts(int value);
+        ISettings SetMinNumber(int value);
+        ISettings SetMaxNumber(int value);
+    }
+}
---
+++ Otus.Solid/Game/GameSettings.cs
@@ -0,0 +1,32 @@
+﻿using Otus.Solid.Abstractions;
+using System;
+
+namespace Otus.Solid.Game
+{
+    public class GameSettings : ISettings
+    {
+        public int TotalNumberAttempts { get; private set; } = 5;
+        public int MinNumber { get; private set; } = 0;
+        public int MaxNumber { get; private set; } = 100;
+
+        public ISettings SetTotatNumberAttempts(int value)
+        {
+            TotalNumberAttempts = value;
+            return this;
+        }
+
+        public ISettings SetMinNumber(int value)
+        {
+            if (value >= MaxNumber) throw new Exception("Минимальное число равно или больше максимального");
+            MinNumber = value;
+            return this;
+        }
+
+        public ISettings SetMaxNumber(int value)
+        {
+            if (value <= MinNumber) throw new Exception("Максимальное число равно или меньше минимального");
+            MaxNumber = value;
+            return this;
+        }
+    }
+}
